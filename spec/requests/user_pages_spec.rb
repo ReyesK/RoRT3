44be62fbca77962afb61db1548d6ff63ce824f1b
Kyle Reyes <mr.kylereyes@gmail.com>
2013-02-21 19:59:07 -0500

    Finish sign up

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,56 +1,63 @@
 require 'spec_helper'
 
-describe "User pages" do 
-	subject { page }
+describe "User pages" do
 
-	describe "signup page" do
-		before{ visit signup_path }
+  subject { page }
 
-		it { should have_selector('h1', :text=> 'Sign up') }
-		it { should have_selector('title', :text=> full_title('Sign up'))}
-	end
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-	describe "profile page" do
-		let(:user) { FactoryGirl.create(:user) }
-		before{ visit user_path(user) }
+    it { should have_selector('h1', text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
 
-		it { should have_selector('h1', text: user.name) }
-		it { should have_selector('title', text: user.name) }
-	end
+  describe "signup page" do
+    before { visit signup_path }
 
-	describe "signup" do 
-		before { visit signup_path }
-		let(:submit) { "Create my account" }
+    it { should have_selector('h1', text: 'Sign up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
+  end
 
-		describe "with invalid information" do 
-			it "should not create a user" do 
-				expect { click_button submit }.to change(User, :count).by(0)
-			end
-		end
+  describe "signup" do
 
-		describe "with valid information" do 
-			before do 
-				fill_in "Name", with: "Example User"
-				fill_in "Email", with: "user@example.com"
-				fill_in "Password", with: "foobar"
-				fill_in "Confirmation", with: "foobar"
-			end
-			it "should create a user" do
-				expect { click_button submit}.to change(User, :count).by(1)
-			end
-			describe "after submission" do 
-				before { click_button submit }
+    before { visit signup_path }
 
-				it { should have_selector('title', text: 'Sign up') }
-				it { should have_content('error') }
-			end
-			describe "after saving the user" do
-				before { click_button submit }
-				let(:user) { User.find_by_email('user@example.com') }
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button "Create my account" }.not_to change(User, :count)
+      end
+      
+      describe "error messages" do
+        before { click_button "Create my account" }
 
-				it { should have_selector('title', text: user.name) }
-				it { should have_selector('div.alert.alert-sucess', text: 'Welcome') }
-			end
-		end
-	end
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect do
+          click_button "Create my account"
+        end.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button "Create my account" }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
+    end
+  end
 end
